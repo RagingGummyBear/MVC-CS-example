f2db0b74821a6b13cfdade389a519fde4ea5f4e3
Darko Orovchanec <darko.orovchanec@seavus.com>
2019-04-24 09:14:52 +0200

    Initial Commit

---
+++ MVC-CS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MVC-CS
+//
+//  Created by Seavus on 4/24/19.
+//  Copyright © 2019 Seavus. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
